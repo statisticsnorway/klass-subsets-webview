d629fceacf3a08d2b8391c9aa52f7ffd954b462f
Olve Drageset <olve.drageset@gmail.com>
2021-02-09 12:05:09 +0100

    log instead of allert when stat units are missing

--- src/scripts/loadSubset.js
+++ src/scripts/loadSubset.js
@@ -82,8 +82,12 @@ function displaySubsetSeriesInformation(subsetSeries, language, baseURL, default
     if (Array.isArray(subsetSeries["statisticalUnits"]))
         statUnitsValue = subsetSeries["statisticalUnits"].toString();
     else {
-        alert("subsetSeries['statisticalUnits'] was not an array. The type was "+subsetSeries["statisticalUnits"]+". Setting to empty array.");
         statUnitsValue = [];
+        if (!subsetSeries.hasOwnProperty("statisticalUnits")) {
+            console.log("The subsetSeries has no 'statisticalUnits' field present");
+        } else {
+            console.log("subsetSeries['statisticalUnits'] was not an array. The type was "+subsetSeries["statisticalUnits"]+". Setting to empty array.");
+        }
     }
     console.log("statUnitsValue: "+statUnitsValue);
     statUnitElement.appendChild(document.createTextNode(statUnitsValue));
